c48410a4f357c1ad3444165b43b13ee18ae56935
malulattaa <maria.latta2@estudante.ifms.edu.br>
2026-06-02 16:21:04 -0400

    feat: add usuario model

---
+++ reddif-project/backend/app/models/usuario.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, Integer, String, DateTime, Enum
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database import Base
+import enum
+
+class TipoUsuario(enum.Enum):
+    aluno = "aluno"
+    monitor = "monitor"
+    professor = "professor"
+    admin = "admin"
+    
+class CursoUsuario(enum.Enum):
+    eletrotecnica = "Técnico em eletrotécnica"
+    informatica = "Técnico em Informática"
+    ads = "Análise e Desenvolvimento de Sistemas"
+    engComputacao = "Engenharia da Computação"
+    engAutomacao = "Engenharia de Controle e Automação"
+    adm = "Administração"
+    pos = "Pós Graduação"
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False, index=True)
+    senha = Column(String, nullable=False)
+    curso = Column(Enum(CursoUsuario), nullable=False)
+    tipo = Column(Enum(TipoUsuario), default=TipoUsuario.aluno, nullable=False)
+    pontos = Column(Integer, default=0)
+    criado_em = Column(DateTime, default=datetime.utcnow)
+
+    posts = relationship("Post", back_populates="usuario")
+    respostas = relationship("Resposta", back_populates="usuario")
+    conquistas = relationship("UsuarioConquista", back_populates="usuario")
