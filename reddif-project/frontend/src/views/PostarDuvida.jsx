c5388c53ad746dc27e001de3a9707157482e239c
Moxxie <anaclaramiguel567@gmail.com>
2026-06-02 21:10:33 -0400

    feat:add postar duvidas form and tips components

--- reddif-project/frontend/src/views/PostarDuvida.jsx
+++ reddif-project/frontend/src/views/PostarDuvida.jsx
@@ -1,3 +1,16 @@
+import Navbar from '../components/Navbar'
+import PostarDuvidaForm from '../components/PostarDuvidaForm'
+import DicasDuvida from '../components/DicasDuvida'
+
 export default function PostarDuvida() {
-  return <h1>Postar Dúvida</h1>
+  return (
+    <div className="min-h-screen bg-[#faf7f4]">
+      <Navbar />
+
+      <main className="max-w-3xl mx-auto py-8 px-4 flex flex-col gap-6">
+        <PostarDuvidaForm />
+        <DicasDuvida />
+      </main>
+    </div>
+  )
 }
