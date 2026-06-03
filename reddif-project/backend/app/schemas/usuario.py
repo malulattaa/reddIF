e9c8270b8abb69ae09bdb1ff49626f95ac02ca1c
malulattaa <maria.latta2@estudante.ifms.edu.br>
2026-06-02 22:22:34 -0400

    feat: add usuario schemas

---
+++ reddif-project/backend/app/schemas/usuario.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from app.models.usuario import TipoUsuario, CursoUsuario
+
+class UsuarioCreate(BaseModel):
+    nome: str
+    email: EmailStr
+    senha: str
+    curso: CursoUsuario
+    
+class UsuarioLogin(BaseModel):
+    email: EmailStr
+    senha: str
+    
+class UsuarioResponse(BaseModel):
+    id: int
+    nome: str
+    email: EmailStr
+    curso: CursoUsuario
+    tipo: TipoUsuario
+    pontos: int
+    criado_em: datetime
+    
+    class Config: 
+        from_attributes = True
+        
